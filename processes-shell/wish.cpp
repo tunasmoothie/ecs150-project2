0ae56d4cf690209446c6fbc08d33e746c0c9d7eb
tunasmoothie <tunasmoothie@gmail.com>
2021-04-13 04:26:41 -0700

    variable name changes & function setup preparing for parsing

--- processes-shell/wish.cpp
+++ processes-shell/wish.cpp
@@ -9,77 +9,82 @@
 #include <errno.h>
 
 
-std::vector<std::string> parseCmd(std::string cmd_raw){
-	std::istringstream ss(cmd_raw);
-	std::vector<std::string> parsedCmd;
+std::vector<std::string> convertInputToStr(std::string input_raw){
+	std::istringstream ss(input_raw);
+	std::vector<std::string> converted;
 	std::string word;
 	while(ss >> word){
-		parsedCmd.push_back(word);
+		converted.push_back(word);
 	}
-	return parsedCmd;
+	return converted;
+}
+
+std::vector<std::vector<std::string>> parseLine(std::vector<std::string>){
+	std::vector<std::vector<std::string>> parsedLine;
+	return parsedLine;
 }
 
 int main(int argc, char *argv[]){
 	
-	std::string cmd_raw;
-	std::vector<std::string> cmd_parsed;
+	std::string input_raw;
+	std::vector<std::string> input_str;
 	std::vector<std::string> paths;
 	paths.push_back("");
 	paths.push_back("/bin/");
 	
 	// ========================================= command interpretation lambda =========================================
 	auto interpretCmd = [&](){
-		cmd_parsed = parseCmd(cmd_raw);
-		if(cmd_parsed.size() == 0);
-			else if(cmd_parsed[0] == "exit"){
-				if(cmd_parsed.size() != 1)
+		input_str = convertInputToStr(input_raw);
+		if(input_str.size() == 0);
+			else if(input_str[0] == "exit"){
+				if(input_str.size() != 1)
 					std::cerr << "An error has occurred" << std::endl;
 				else
 					exit(0);
 			}
-			else if(cmd_parsed[0] == "path"){
+			else if(input_str[0] == "path"){
 				paths.clear();
 				paths.push_back("");
-				for(size_t i = 1; i < cmd_parsed.size(); i++){
-					if(cmd_parsed[i][0] != '/')
-						cmd_parsed[i] = "/" + cmd_parsed[i];
-					if(cmd_parsed[i][cmd_parsed[i].size()-1] != '/')
-						cmd_parsed[i] += "/";
+				for(size_t i = 1; i < input_str.size(); i++){
+					if(input_str[i][0] != '/')
+						input_str[i] = "/" + input_str[i];
+					if(input_str[i][input_str[i].size()-1] != '/')
+						input_str[i] += "/";
 					char tmp[2048];
 					getcwd(tmp, 2048);
-					cmd_parsed[i] = tmp + cmd_parsed[i];
-					paths.push_back(cmd_parsed[i]);
+					input_str[i] = tmp + input_str[i];
+					paths.push_back(input_str[i]);
 				}
 			}
-			else if(cmd_parsed[0] == "cd"){
-				if(cmd_parsed.size() != 2)
+			else if(input_str[0] == "cd"){
+				if(input_str.size() != 2)
 					std::cerr << "An error has occurred" << std::endl;
-				else if(chdir((char*)cmd_parsed[1].c_str()) == 0);
+				else if(chdir((char*)input_str[1].c_str()) == 0);
 				else
 					std::cerr << "An error has occurred" << std::endl;
 			}
 			else{
 				pid_t pid = fork();
 				if(pid == 0){
-					char *args[cmd_parsed.size() + 1];
+					char *args[input_str.size() + 1];
 					
 					// string to char array conversion for args
-					for(size_t i = 0; i < cmd_parsed.size(); i++){
-						args[i] = new char[cmd_parsed[i].size() + 1];
-						std::strcpy(args[i], cmd_parsed[i].c_str());
+					for(size_t i = 0; i < input_str.size(); i++){
+						args[i] = new char[input_str[i].size() + 1];
+						std::strcpy(args[i], input_str[i].c_str());
 					}
-					args[cmd_parsed.size()] = NULL;
+					args[input_str.size()] = NULL;
 					
 					for(size_t i = 0; i < paths.size(); i++){
-						char cmd_path[paths[i].size() + cmd_parsed[0].size() + 1];
-						std::strcpy(cmd_path, (paths[i] + cmd_parsed[0]).c_str());
+						char cmd_path[paths[i].size() + input_str[0].size() + 1];
+						std::strcpy(cmd_path, (paths[i] + input_str[0]).c_str());
 						execv(cmd_path, args);   // exec() will kill process if command success.
 					}
 					std::cerr << "An error has occurred" << std::endl;
 					exit(1);
 				}	
 				else{
-					pid_t wait = wait(NULL);
+					wait(NULL);
 				}
 				
 			}
@@ -89,7 +94,7 @@ int main(int argc, char *argv[]){
 	if(argc == 1){
 		while(1){
 			std::cout << "wish> ";
-			std::getline(std::cin, cmd_raw);
+			std::getline(std::cin, input_raw);
 			interpretCmd();
 		}
 	}
@@ -97,7 +102,7 @@ int main(int argc, char *argv[]){
 	else if(argc == 2){
 		std::ifstream batchFile;
 		batchFile.open(argv[1]);
-		while(std::getline(batchFile, cmd_raw)){
+		while(std::getline(batchFile, input_raw)){
 			interpretCmd();
 		}
 		batchFile.close();
