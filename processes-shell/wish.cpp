a1e6974e959532591bec2882ded08f3967999b03
tunasmoothie <tunasmoothie@gmail.com>
2021-04-13 05:41:57 -0700

    test 8 passed

--- processes-shell/wish.cpp
+++ processes-shell/wish.cpp
@@ -8,6 +8,9 @@
 #include <sys/wait.h>
 #include <errno.h>
 
+void printError(){
+	std::cerr << "An error has occurred" << std::endl;
+}
 
 std::vector<std::string> convertInputToStr(std::string input_raw){
 	std::istringstream ss(input_raw);
@@ -19,13 +22,30 @@ std::vector<std::string> convertInputToStr(std::string input_raw){
 	return converted;
 }
 
-std::vector<std::vector<std::string>> parseLine(std::vector<std::string>){
+std::vector<std::vector<std::string>> parseLine(std::vector<std::string> line, int &mode){
 	std::vector<std::vector<std::string>> parsedLine;
+	int index = 0;
+	std::vector<std::string> tmp;
+	parsedLine.push_back(tmp);
+	// 0 = Normal, 1 = Redirect, 2 = Parallel
+	mode = 0;
+	for(size_t i = 0; i < line.size(); i ++){
+		parsedLine[index].push_back(line[i]);
+		if(line[i] == ">"){
+			mode = (mode == 1) ? (printError(), exit(1), 0) : 1;
+		    parsedLine.push_back(tmp);
+			index++;
+		}	
+	}
+
+	
+	if(parsedLine.back().empty())
+		parsedLine.pop_back();
+	
 	return parsedLine;
 }
 
 int main(int argc, char *argv[]){
-	
 	std::string input_raw;
 	std::vector<std::string> input_str;
 	std::vector<std::string> paths;
@@ -36,34 +56,48 @@ int main(int argc, char *argv[]){
 	auto interpretCmd = [&](){
 		input_str = convertInputToStr(input_raw);
 		if(input_str.size() == 0);
-			else if(input_str[0] == "exit"){
-				if(input_str.size() != 1)
-					std::cerr << "An error has occurred" << std::endl;
-				else
-					exit(0);
+		else if(input_str[0] == "exit"){
+			if(input_str.size() != 1)
+				printError();
+			else
+				exit(0);
+		}
+		// -------- built-in commands --------
+		else if(input_str[0] == "path"){
+			paths.clear();
+			paths.push_back("");
+			for(size_t i = 1; i < input_str.size(); i++){
+				if(input_str[i][0] != '/')
+					input_str[i] = "/" + input_str[i];
+				if(input_str[i][input_str[i].size()-1] != '/')
+					input_str[i] += "/";
+				char tmp[2048];
+				getcwd(tmp, 2048);
+				input_str[i] = tmp + input_str[i];
+				paths.push_back(input_str[i]);
 			}
-			else if(input_str[0] == "path"){
-				paths.clear();
-				paths.push_back("");
-				for(size_t i = 1; i < input_str.size(); i++){
-					if(input_str[i][0] != '/')
-						input_str[i] = "/" + input_str[i];
-					if(input_str[i][input_str[i].size()-1] != '/')
-						input_str[i] += "/";
-					char tmp[2048];
-					getcwd(tmp, 2048);
-					input_str[i] = tmp + input_str[i];
-					paths.push_back(input_str[i]);
+		}
+		else if(input_str[0] == "cd"){
+			if(input_str.size() != 2)
+				printError();
+			else if(chdir((char*)input_str[1].c_str()) == 0);
+			else
+				printError();
+		}
+		// -------- external commands --------
+		else{
+			// 0 = Normal, 1 = Redirect, 2 = Parallel
+			int mode = 0;
+			std::vector<std::vector<std::string>> commands = parseLine(input_str, mode);
+			/*
+			for(auto a : commands){
+				for(auto b : a){
+					std::cout << b;
 				}
-			}
-			else if(input_str[0] == "cd"){
-				if(input_str.size() != 2)
-					std::cerr << "An error has occurred" << std::endl;
-				else if(chdir((char*)input_str[1].c_str()) == 0);
-				else
-					std::cerr << "An error has occurred" << std::endl;
-			}
-			else{
+				std::cout << std::endl;
+			}*/
+			
+			if(mode == 0){
 				pid_t pid = fork();
 				if(pid == 0){
 					char *args[input_str.size() + 1];
@@ -78,16 +112,25 @@ int main(int argc, char *argv[]){
 					for(size_t i = 0; i < paths.size(); i++){
 						char cmd_path[paths[i].size() + input_str[0].size() + 1];
 						std::strcpy(cmd_path, (paths[i] + input_str[0]).c_str());
-						execv(cmd_path, args);   // exec() will kill process if command success.
+						execv(cmd_path, args);  
+						// exec() will cut subsequent error handling if command succeeds.
 					}
-					std::cerr << "An error has occurred" << std::endl;
+					printError();
 					exit(1);
 				}	
 				else{
 					wait(NULL);
 				}
-				
+			}	
+			else if(mode == 1){
+				if(commands.size() != 2){
+					printError();
+					exit(0);  //unsure
+				}
+				std::cout << "Redirect" << std::endl; //placeholder
 			}
+		}
+	    
 	};
 	
 	// ============================================= interactive mode =============================================
