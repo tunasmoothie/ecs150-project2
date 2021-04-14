d244e0b06dd713dbdb4a688ce81fb6077ae459fc
tunasmoothie <tunasmoothie@gmail.com>
2021-04-13 18:40:26 -0700

    redirection works, test 12 passed

--- processes-shell/wish.cpp
+++ processes-shell/wish.cpp
@@ -13,7 +13,27 @@ void printError(){
 	std::cerr << "An error has occurred" << std::endl;
 }
 
-std::vector<std::string> convertInputToStr(std::string input_raw){
+void executeCmd(const std::vector<std::string> &cmd_str, const std::vector<std::string> &paths){
+	char *args[cmd_str.size() + 1];
+					
+	// string to char array conversion for args
+	for(size_t i = 0; i < cmd_str.size(); i++){
+		args[i] = new char[cmd_str[i].size() + 1];
+		std::strcpy(args[i], cmd_str[i].c_str());
+	}
+	args[cmd_str.size()] = NULL;
+					
+	for(size_t i = 0; i < paths.size(); i++){
+		char cmd_path[paths[i].size() + cmd_str[0].size() + 1];
+		std::strcpy(cmd_path, (paths[i] + cmd_str[0]).c_str());
+		execv(cmd_path, args); 
+		// exec() will cut subsequent error handling if command succeeds.
+	}
+	printError();
+	exit(1);
+}
+
+std::vector<std::string> convertInputToStrVec(std::string input_raw){
 	std::istringstream ss(input_raw);
 	std::vector<std::string> converted;
 	std::string word;
@@ -23,9 +43,9 @@ std::vector<std::string> convertInputToStr(std::string input_raw){
 	return converted;
 }
 
-std::vector<std::vector<std::string>> parseLine(std::vector<std::string> line, int &mode){
-	
+std::vector<std::vector<std::string>> processLine(std::vector<std::string> line, int &mode){
 	std::vector<std::vector<std::string>> output;
+	
 	// 0 = Normal, 1 = Redirect, 2 = Parallel
 	mode = 0;
 	
@@ -74,40 +94,40 @@ std::vector<std::vector<std::string>> parseLine(std::vector<std::string> line, i
 
 int main(int argc, char *argv[]){
 	std::string input_raw;
-	std::vector<std::string> input_str;
+	std::vector<std::string> input_str_vec;
 	std::vector<std::string> paths;
 	paths.push_back("");
 	paths.push_back("/bin/");
 	
 	// ========================================= command interpretation lambda =========================================
 	auto interpretCmd = [&](){
-		input_str = convertInputToStr(input_raw);
-		if(input_str.size() == 0);
-		else if(input_str[0] == "exit"){
-			if(input_str.size() != 1)
+		input_str_vec = convertInputToStrVec(input_raw);
+		if(input_str_vec.size() == 0);
+		else if(input_str_vec[0] == "exit"){
+			if(input_str_vec.size() != 1)
 				printError();
 			else
 				exit(0);
 		}
 		// -------- built-in commands --------
-		else if(input_str[0] == "path"){
+		else if(input_str_vec[0] == "path"){
 			paths.clear();
 			paths.push_back("");
-			for(size_t i = 1; i < input_str.size(); i++){
-				if(input_str[i][0] != '/')
-					input_str[i] = "/" + input_str[i];
-				if(input_str[i][input_str[i].size()-1] != '/')
-					input_str[i] += "/";
+			for(size_t i = 1; i < input_str_vec.size(); i++){
+				if(input_str_vec[i][0] != '/')
+					input_str_vec[i] = "/" + input_str_vec[i];
+				if(input_str_vec[i][input_str_vec[i].size()-1] != '/')
+					input_str_vec[i] += "/";
 				char tmp[2048];
 				getcwd(tmp, 2048);
-				input_str[i] = tmp + input_str[i];
-				paths.push_back(input_str[i]);
+				input_str_vec[i] = tmp + input_str_vec[i];
+				paths.push_back(input_str_vec[i]);
 			}
 		}
-		else if(input_str[0] == "cd"){
-			if(input_str.size() != 2)
+		else if(input_str_vec[0] == "cd"){
+			if(input_str_vec.size() != 2)
 				printError();
-			else if(chdir((char*)input_str[1].c_str()) == 0);
+			else if(chdir((char*)input_str_vec[1].c_str()) == 0);
 			else
 				printError();
 		}
@@ -115,59 +135,19 @@ int main(int argc, char *argv[]){
 		else{
 			// 0 = Normal, 1 = Redirect, 2 = Parallel
 			int mode = 0;
-			parseLine(input_str, mode);	
-			
-			/*
-			for(auto a : commands){
-				for(auto b : a){
-					std::cout << b << "  ";
-				}
-				std::cout << std::endl;
-			}*/
+			auto processed_line = processLine(input_str_vec, mode);	
 			
 			pid_t pid = fork();
 			if(pid == 0){
 				if(mode == 0){
-					char *args[input_str.size() + 1];
-					
-					// string to char array conversion for args
-					for(size_t i = 0; i < input_str.size(); i++){
-						args[i] = new char[input_str[i].size() + 1];
-						std::strcpy(args[i], input_str[i].c_str());
-					}
-					args[input_str.size()] = NULL;
-					
-					for(size_t i = 0; i < paths.size(); i++){
-						char cmd_path[paths[i].size() + input_str[0].size() + 1];
-						std::strcpy(cmd_path, (paths[i] + input_str[0]).c_str());
-						execv(cmd_path, args); 
-						// exec() will cut subsequent error handling if command succeeds.
-					}
-					printError();
-					exit(1);
+					executeCmd(input_str_vec, paths);
 				}	
 				else if(mode == 1){
-					/*
-					int fd = open(commands[0][2].c_str(), O_RDWR | O_CREAT);
+					int fd = open(processed_line[1][0].c_str(), O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR);
 					dup2(fd, 1);
-					dup2(fd, 2);
-					//close(fd);
-					
-					char *args[commands[0].size() + 1];
-					// string to char array conversion for args
-					for(size_t i = 0; i < commands[0].size(); i++){
-						args[i] = new char[commands[0][i].size() + 1];
-						std::strcpy(args[i], commands[0][i].c_str());
-					}
-					args[input_str.size()] = NULL;
-					for(size_t i = 0; i < paths.size(); i++){
-						char cmd_path[paths[i].size() + input_str[0].size() + 1];
-						std::strcpy(cmd_path, (paths[i] + input_str[0]).c_str());
-						execv(cmd_path, args);  
-						// exec() will cut subsequent error handling if command succeeds.
-					}
-					printError();
-					exit(1);*/
+					close(fd);
+					executeCmd(processed_line[0], paths);
+					exit(0);
 				}
 			}	
 			else{
